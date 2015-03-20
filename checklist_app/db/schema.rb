6a8578b95f53bee5ac26ca6d922712646b43b23c
Sadiq Okocha <sokocha@gmail.com>
2015-03-20 13:06:15 +0000

    runs migrations for various models

--- checklist_app/db/schema.rb
+++ checklist_app/db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320125927) do
+ActiveRecord::Schema.define(version: 20150320130512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "status",     default: true
+    t.integer  "list_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
